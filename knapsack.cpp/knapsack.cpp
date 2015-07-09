8c08e853a1e5e8c229a6a4525ca7dec5788a0f86
Akshay <sundust.akshay@gmail.com>
2015-07-09 14:38:34 +0530

    edit

--- knapsack.cpp/knapsack.cpp
+++ knapsack.cpp/knapsack.cpp
@@ -4,15 +4,16 @@ using namespace std;
 
 struct attr
 {
-	int upvotes[1000]; //upvotes
-	int cash; //Balance
-	int inv[1000]; //Inventory of each item
-	int price[1000]; //price of each item
+	int upvotes[1000]; 							//upvotes
+	int cash; 									//Balance
+	int inv[1000]; 								//Inventory of each item
+	int price[1000]; 							//price of each item
+	int id[1000];								//id of item
+	int reco[1000];								//final recommended list
 
-};
-
-//2 attributes to be taken care of : upvotes and cash upper limit.
-int knapSack(attr a, int n, int i)
+};									
+												
+void knapSack(attr a, int n, int i)				//2 attributes to be taken care of : upvotes and cash upper limit.
 {
 	float norm[1000];
 	for(j=0;j<n;j++)
@@ -21,9 +22,9 @@ int knapSack(attr a, int n, int i)
 	}
 
 
-	for(i=1;i<=n-1;i++)
+	for(int i=1;i<=n-1;i++) 					//sort according to the normalised value
 	{
-		for(j=1;j<=n-1;j++)
+		for(int j=1;j<=n-1;j++)
 		{
 			if(norm[j] > norm[j+1])
 			{
@@ -42,10 +43,33 @@ int knapSack(attr a, int n, int i)
 				temp=a[i].upvotes;       
 				a[i].upvotes=a[i].upvotes;
 				a[i].upvotes=temp;
+
+				temp=a[i].id;       
+				a[i].id=a[i].id;
+				a[i].id=temp;
 			}	
 		}	
 	}
-
+	int j=0,k=0;
+	while(a[i].cash > 0)
+	{
+		if(a[i].price[j] <= a[i].cash)
+		{
+			a[i].cash= a[i].cash-a[i].price[j];
+			a[i].reco[k] = a[i].id[j];
+			k++;
+			j++;									//Assuming one order of each item
+		}
+		else 
+		{
+			j++;
+		}
+	}
+	cout<<endl;
+	for(int p=0;p<k;p++)
+	{
+		cout<<a[i].reco[p]<<endl;
+	}
 }
 
 
@@ -63,6 +87,10 @@ int main()
 
 	for(int i=0;i<a2;i++)
 	{
+		cout<<"Upvotes :";
+		for(int j=0; j<a2;j++)
+			cin>>a[i].id[j];
+		
 		cout<<"Upvotes :";
 		for(int j=0; j<a2;j++)
 			cin>>a[i].upvotes[j];
